c4b044d7bc424e5046093c713099bee8e86fa559
Sebastián La Rosa <87385140+slarosa-redwood@users.noreply.github.com>
2022-09-14 19:04:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log(this);
+this !== this.top && this.top.postMessage({
+    type: "actionCallback",
+    name: "onActionDisplayed",
+    signature: "s"
+}, "https://videos.redwood.com");
+
+this.addEventListener("message", event => {
+    if (event.origin !== "https://videos.redwood.com")
+        return;
+
+    console.log(event.data);
+});
